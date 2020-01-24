4fc5ec5367c87bf759bd6de0d28818efd55b3935
Your Name <you@example.com>
2020-01-24 20:05:17 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the Git repository for gridsome.org. Feel free to contribute 🥳
--- src/pages/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Layout>
-    <Section class="text-center">
-      <h1>404 - Page not found</h1>
-    </Section>
-  </Layout>
-</template>
